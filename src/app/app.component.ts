b52791cb53d98a671e337d26d3f46044b630f090
coderjannat <jannatrumi60@gmail.com>
2023-03-06 18:44:37 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'form';
+  postTitle:string;
+  postDetail:string;
+  imageURL: string;
+  postURL: string;
+  addBackground:boolean;
 }
